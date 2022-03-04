f280ab9fbe8e93908c7e23c1ae9573e8ee841b39
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-03-04 11:36:17 +0100

    Implement Python Connector Module

--- init_env.R
+++ init_env.R
@@ -6,6 +6,8 @@
 
 # -- Declare path
 path <- list(script = "./src/script/R",
+             python_script = "./src/script/Python",
+             resource = "./src/resources",
              data = "./dataset",
              formated = "./dataset/formated",
              processed = "./dataset/processed",
@@ -20,16 +22,3 @@ file <- list(raw_dataset = file.path(path$data, "sydney_2008_2017_raw.csv"),
              model_list = "model_list.csv",
              formated = "formated_data.csv")
 
-
-# --------------------------------------------------------------------------------
-# SOURCES
-# --------------------------------------------------------------------------------
-
-# source("D:/Work/R/Library/Correlation/correlation.R")
-# source("D:/Work/R/Library/Correlation/getLinearModel.R")
-# source("D:/Work/R/Library/NaN/na_tools.R")
-# source("D:/Work/R/Library/Stats/meanByGroup.R")
-# source("D:/Work/R/Library/Dataframe/dataframe.R")
-# source("D:/Work/R/Library/Plot/plot_tools.R")
-
-#library(kangarooAI)
--- server.R
+++ server.R
@@ -6,6 +6,7 @@
 # -- Library
 
 library(shiny)
+library(kpython)
 
 # -- Init env
 source("./init_env.R")
@@ -53,13 +54,19 @@ shinyServer(
         output$version_timestamp <- renderText({paste("Version:", app_version[[1]])})
         output$version_comment <- renderText(app_version[[2]])
         
+        # -- call back-end modules
+        pythonConnector_Server("python", script_path =  path$python_script, dependencies = path$resource)
         
-        # -- declare modules
+        # -- call application modules
         map_Server("map")
         datasetManager_Server("model", r, path, file)
         analysisManager_Server("analyze", r)
         nanManager_Server("nan", r)
         reccurentCheck_Server("check", r)
+        
+        res <- k_poc_add(5,2)
+        cat("Python call = ", res, "\n")
+        #showNotification(paste("Python call = ", res))
         
     }
 )
