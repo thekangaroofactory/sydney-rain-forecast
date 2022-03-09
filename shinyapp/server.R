6e3b4f98d7766447bf0e33de8fdedb6158463682
kangarooaifr <philippe.peret@kangaroo-ai.fr>
2022-03-09 14:28:40 +0100

    Deliver Python integration

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -4,7 +4,6 @@
 # --------------------------------------------------------------------------------
 
 # -- Library
-
 library(shiny)
 library(kpython)
 library(reticulate)
@@ -58,31 +57,9 @@ shinyServer(
     output$version_timestamp <- renderText({paste("Version:", app_version[[1]])})
     output$version_comment <- renderText(app_version[[2]])
     
+    
     # -- call back-end modules
-    #pythonConnector_Server("python", script_path =  path$python_script, dependencies = path$resource)
-    
-    
-    output$list_dir <- renderUI(
-      HTML(paste0(list.dirs(input$my_input, recursive = F), collapse = '<br/>')))
-
-    output$list_file <- renderUI(
-      HTML(paste0(list.files(path = input$my_input, all.files = TRUE, include.dirs = FALSE), collapse = '<br/>')))
-      
-    # System path to python
-    output$which_python <- renderText({
-      paste0('Python path: ', Sys.which('python'))
-    })
-    
-    # Python version
-    output$python_version <- renderUI({
-      rr = reticulate::py_discover_config(use_environment = 'python35_env')
-      #paste0('Python version: ', rr$version)
-      HTML(paste0(rr, collapse = '<br/>'))
-    })
-    
-    
-    #reticulate::use_virtualenv("r-reticulate", required = T)
-    source_python("./src/script/Python/add.py", envir = globalenv())
+    pythonConnector_Server("python", script_path = path$python_script, skip_init = TRUE)
     
     
     # -- call application modules
@@ -92,9 +69,12 @@ shinyServer(
     nanManager_Server("nan", r)
     reccurentCheck_Server("check", r)
     
+    
+    # -- check python
     res <- k_poc_add(5,2)
     cat("Python call = ", res, "\n")
     showNotification(paste("Python call = ", res))
-    
+        
+
   }
 )
